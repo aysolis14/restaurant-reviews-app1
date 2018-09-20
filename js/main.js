15c6523be12d542f494bf8bc69e7e101be3113b1
Amanda Solis <asolis@Amandas-MacBook-Pro.local>
2018-09-20 16:30:46 -0500

    Added service worker and accessibility features

--- js/main.js
+++ js/main.js
@@ -178,6 +178,7 @@ createRestaurantHTML = (restaurant) => {
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
+  more.tabIndex = '3';
   li.append(more)
 
   return li
@@ -209,3 +210,10 @@ addMarkersToMap = (restaurants = self.restaurants) => {
   });
 } */
 
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+  .register('/sw.js')
+  .catch(function(err) {
+    console.error(err);
+  });
+}
